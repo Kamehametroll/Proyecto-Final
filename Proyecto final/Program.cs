2043f19b4699c2cc0143619903ddd0c16484418e
Kamehametroll <34353754+Kamehametroll@users.noreply.github.com>
2018-11-10 18:28:14 -0400

    0.1
    Clase principal del programa.

---
+++ Proyecto final/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class cancion
+        {
+            public cancion(string name, string alb, string artist, string genere, int duration) // El constructor de la clase
+            {
+                this.nombre = name;
+                this.album = alb;
+                this.artista = artist;
+                this.genero = genere;
+                this.duracion = duration;
+            }
+            public string nombre;
+            public int id;
+            public string album;
+            public string artista;
+            public string genero;
+            public string formato;
+            public int duracion; 
+        }
+    }
+}
